d3d9482f3cd916e10f131ea911e4b81cd992b2e9
uttamtamang <uttamtamang45@gmail.com>
2020-01-27 23:48:46 +0545

    login successful

--- app/src/main/java/com/example/meroghar/LoginActivity.java
+++ app/src/main/java/com/example/meroghar/LoginActivity.java
@@ -14,14 +14,21 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.eightbitlab.supportrenderscriptblur.SupportRenderScriptBlur;
+import com.example.meroghar.Interfaces.UserApi;
+import com.example.meroghar.Models.User;
+import com.example.meroghar.URL.Url;
 
 //import butterknife.BindView;
 import eightbitlab.com.blurview.BlurView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText userName, password;
+    EditText userName, userPassword;
     Button btnLogin,btnSignup;
     TextView registerNow;
 
@@ -47,6 +54,8 @@ public class LoginActivity extends AppCompatActivity {
 //            getWindow().setStatusBarColor(Color.TRANSPARENT);
 //        }
 
+        userName = findViewById(R.id.userEmail);
+        userPassword = findViewById(R.id.userPassword);
 
         btnLogin= findViewById(R.id.btnLogin);
         registerNow = findViewById(R.id.linkRegistration);
@@ -68,6 +77,13 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
 //        final float radius = 25f;
 //        final Drawable windowBackground = getWindow().getDecorView().getBackground();
 //        blur_email.setupWith(root)
@@ -76,5 +92,39 @@ public class LoginActivity extends AppCompatActivity {
 //                .setBlurRadius(radius)
 //                .setHasFixedTransformationMatrix(true);
 
+    }
+    public void login(){
+        String email, password;
+
+        email= userName.getText().toString();
+        password= userPassword.getText().toString();
+
+        User user = new User(email, password);
+        UserApi userApi = Url.getInstance().create(UserApi.class);
+        Call<Void> login = userApi.login(user);
+
+        login.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(LoginActivity.this, "Login Successful.", Toast.LENGTH_SHORT).show();
+                openDashBoard();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, "Error " + t.getLocalizedMessage() , Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+    public void openDashBoard(){
+        Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+        startActivity(intent);
     }
 }
