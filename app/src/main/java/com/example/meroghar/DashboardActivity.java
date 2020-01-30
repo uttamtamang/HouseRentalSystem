0756ab33b798bfe2d051909f42b2661562389ec0
uttamtamang <uttamtamang45@gmail.com>
2020-01-30 16:37:41 +0545

    Nav drawer

--- app/src/main/java/com/example/meroghar/DashboardActivity.java
+++ app/src/main/java/com/example/meroghar/DashboardActivity.java
@@ -2,30 +2,59 @@ package com.example.meroghar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.meroghar.Fragments.FavoritesFragment;
 import com.example.meroghar.Fragments.DashboardFragment;
 import com.example.meroghar.Fragments.PropertiesFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
         getSupportActionBar().hide();
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListner);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+//                new DashboardFragment()).commit();
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new DashboardFragment()).commit();
     }
     private BottomNavigationView.OnNavigationItemSelectedListener navListner =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,7 +63,7 @@ public class DashboardActivity extends AppCompatActivity {
                     Fragment selectFragment = null;
                     switch (item.getItemId()){
 
-                        case R.id.nav_home:
+                        case R.id.nav_dashboard:
                             selectFragment = new DashboardFragment();
                             break;
                         case R.id.nav_favorite:
@@ -46,16 +75,17 @@ public class DashboardActivity extends AppCompatActivity {
                             break;
 
                         case R.id.nav_profile:
-                            selectFragment = new PropertiesFragment();
-                            break;
+                            drawer.openDrawer(GravityCompat.START);
+                            return true;
 
 
                     }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
                             selectFragment).commit();
                     return true;
                 }
             };
+
 
 
 }
